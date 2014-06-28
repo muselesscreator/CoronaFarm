3bed56b10594e9f10d4294851f3f2e5510e1c2c8
Ben Warzeski <bwarzesk@gmail.com>
2014-06-28 12:42:32 -0400

    fix the things

--- FarmGame/splash.lua
+++ FarmGame/splash.lua
@@ -26,9 +26,12 @@ function scene:createScene( event )
     layers = display.newGroup()
     layers.bg = display.newGroup()
 
-    bg = display.newImage('Default-Landscape.png')
-    bg.anchorX = 0
-    bg.anchorY = 0
+    print('splash')
+
+    bg = display.newImageRect('Default-Landscape.png', display.contentWidth, display.contentHeight)
+    bg.x = display.contentCenterX
+    bg.y = display.contentCenterY
+
     layers.bg:insert(bg)
 
 
